d4007131ec1e788ac652337b86c42c5de8ba2d7c
WevertonW <201.29.002@unirios.edu.br>
2022-09-15 23:46:56 -0300

    Primeiro Commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $eleitor = new Eleitor($_POST['nome'], $_POST['cpf'], $_POST['idade'], $_POST['v
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eleição</title>
+    <title>Eleicao</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css"/>
 
