96df1809cb1ee4b56c9d6cb3ebb7fe879642c6ed
WevertonW <201.29.002@unirios.edu.br>
2022-09-15 23:36:16 -0300

    Primeiro Commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $eleitor = new Eleitor($_POST['nome'], $_POST['cpf'], $_POST['idade'], $_POST['v
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema De Votação</title>
+    <title>Eleição</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css"/>
 
@@ -32,7 +32,7 @@ $eleitor = new Eleitor($_POST['nome'], $_POST['cpf'], $_POST['idade'], $_POST['v
     <div class="container border border-2 rounded-4 p-4 bg-white mb-5" style="max-width: 450px;">
         
         <form method="post">
-            <h1 class="mb-4 text-center">Votação</h1>
+            <h1 class="mb-4 text-center">Eleição</h1>
             <div class="">
                 <div class="mb-3 bs-success">
                     <label for="nome" class="form-label fw-semibold">Nome do eleitor</label>
